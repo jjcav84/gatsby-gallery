784f2c9f8916be41cf47c8783e2e245be2976981
Jacob Cavazos <jjcav84@gmail.com>
2019-08-23 03:53:27 -0500

    installed and configured gatsby-netlify-plugin (#4)

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,7 @@ module.exports = {
         icon: `static/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-netlify`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // "gatsby-plugin-offline",
